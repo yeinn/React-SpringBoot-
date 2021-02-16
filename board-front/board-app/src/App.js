ab32effe8dd841738cc78d94b81aeb7cb03c43e2
10-N3214\User <yichoi97@daewoong.co.kr>
2021-02-16 17:03:20 +0900

    - board-front 작업 완료

--- board-front/board-app/src/App.js
+++ board-front/board-app/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import ListBoardComponent from './components/ListBoardComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent/>
+        <div className="container">
+          <switch>
+            <Route path="/" exact component = {ListBoardComponent}></Route>
+            <Route path="/board" component = {ListBoardComponent}></Route>
+          </switch>
+        </div>
+        <FooterComponent/>
+      </Router>
     </div>
   );
 }
---
+++ board-front/board-app/src/components/ListBoardComponent.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import BoardService from '../service/BoardService';
+
+class ListBoardComponent extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={boards: []}
+    }
+
+    componentDidMount(){
+        BoardService.getBoards().then((res)=>{
+            this.setState({boards: res.data});
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">게시글 목록</h2>
+                <div className="row">
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>타이틀</th>
+                                <th>작성자</th>
+                                <th>작성일</th>
+                                <th>갱신일</th>
+                                <th>좋아요</th>
+                                <th>조회수</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.boards.map(
+                                    board =>
+                                    <tr key={board.no}>
+                                        <td>{board.title}</td>
+                                        <td>{board.memberNo}</td>
+                                        <td>{board.createdTime}</td>
+                                        <td>{board.updatedTime}</td>
+                                        <td>{board.likes}</td>
+                                        <td>{board.counts}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ListBoardComponent;
